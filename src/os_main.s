ba6e0bb8c4b8983fe5fe337138482083e3c85b68
filip-we <filip.wettlen@gmail.com>
2021-02-17 17:12:59 +0100

    Converted print_string subroutine

--- src/os_main.s
+++ src/os_main.s
@@ -2,6 +2,7 @@
     .include "via1.s"
 
     .import __STACK_START__
+    .import __ROM_START__
     ;.import __RAMPROG_LOAD__
 
 ; ----------------------------------------
@@ -11,6 +12,7 @@ COMMAND_RDRAM = $01
 COMMAND_WRRAM = $02
 COMMAND_BOOT = $ff
 
+PROTOCOL_HEADER_LEN = $04
 ; ----------------------------------------
 ; ----- ADDRESSES ------------------------
 ; ----------------------------------------
@@ -18,12 +20,17 @@ STACK = __STACK_START__
 
 COMMAND =             $00
 COMMAND_ADDRESS =     $01
+COMMAND_DATALEN =     $03
+PRINT_HEX_TO_ACIA =   $04
+TEMP =                $05
+TEMP_ADDRESS =        $06     ; 2 bytes
 
 COM_BUF_START =     $0200
 COM_BUF_END =       $0201
 BOOT_MODE =         $0210
 
-COM_BUF =           $0300
+COMMAND_DATA =      $0300
+COM_BUF =           $0400
 
     .segment "CODE"
 
@@ -40,7 +47,8 @@ COM_BUF =           $0300
 ; ----- Reset ----------------------------
 ; ----------------------------------------
 reset:
-    sei
+    sei                     ; Set interrupt flag (== interrupts ignored)
+    cld                     ; Clear decimal flag (== integer mode)
     ldx #$ff                ; Initialize stack pointers for data and hardware stacks
     txs
     lda #$00                ; Initialize CLI-buffer pointer
@@ -48,6 +56,7 @@ reset:
     sta COM_BUF_END
 
     sta BOOT_MODE           ; Set boot mode to standard (=0)
+    sta PRINT_HEX_TO_ACIA
 
 ; VIA setup
     lda #%11111111          ; Set all pins on port B to output
@@ -90,150 +99,167 @@ reset:
 ram_boot_mode:
     lda #$01
     sta BOOT_MODE                   ; RAM-write boot-mode is $01
-    dex
-    dex
-    lda #>string_ram_boot_mode      ; High byte
-    sta 1, x
     lda #<string_ram_boot_mode      ; Low byte
-    sta 0, x
+    sta TEMP_ADDRESS
+    lda #>string_ram_boot_mode      ; High byte
+    sta TEMP_ADDRESS + 1
     jsr print_string
-    inx                             ; Free up data stack
-    inx
     cli
-    ;jmp RAMPROG
-    jmp $1000
-
-;ram_boot_loop:
-;    jmp ram_boot_loop
+ram_boot_loop:
+    jmp ram_boot_loop
 ;    cmp RAM_BOOT_COMMAND_BOOT_NOW
 ;    bne ram_boot_loop
 ;    jmp $1000
+;    jmp RAMPROG
 
 ; ----------------------------------------
 ; ----- Normal boot-mode -----------------
 ; ----------------------------------------
 standard_boot_mode:
-    dex
-    dex
-    lda #>string_standard_boot_mode ; High byte
-    sta 1, x
+    inc PRINT_HEX_TO_ACIA           ; Will also send to ACIA
     lda #<string_standard_boot_mode ; Low byte
-    sta 0, x
+    sta TEMP_ADDRESS
+    lda #>string_standard_boot_mode ; High byte
+    sta TEMP_ADDRESS + 1
+    ldy #$00
     jsr print_string
-    inx                             ; Free up data stack
-    inx
 
     lda #(%10000000 | LCD_SECOND_LINE)
     jsr lcd_send_command
     lda #$00                        ; Initialize communication counter
     tay
     cli                             ; Clear Interrupt disable (i.e. listen for interrupt requests)
-
+    ldy #$00                        ; Count rows on LCD
 main:
     lda COM_BUF_START
     cmp COM_BUF_END
     bpl main                        ; Branch if COM_BUF_START >= COM_BUF_END
 
-    ; y=0: store byte in COMMAND_ADDRESS
-    ; 0<y<=2: store byte in COMMAND
-    ; y>3:  store byte in memory COMMAND_ADDRESS, y
-    ;tya
-    cpy #$00
-    beq main_store_command
-    cpy #$03
-    bmi main_store_address
-main_handle_data:
-    lda COMMAND
-    cmp #COMMAND_RDRAM
-    beq command_rdram
-
-main_loop:
-    inc COM_BUF_START               ; Update COM_BUF_START
-    iny
-    jsr main_print_status
-    cpy #$13
-    bmi main
-    ldy #$00                        ; If y > 12 we roll around to #$00
-    jsr main_print_status
-    jmp main
-
-main_print_status:
-; Print current command, address, and y
+    ; If LCD has 4 or more prints then we clear the display
+    cpy #$04
+    bmi main_print
+    ldy #$00
     lda #%00000001                  ; Clear display
     jsr lcd_send_command
     lda #%00000010                  ; Return cursor home
     jsr lcd_send_command
-
-    ; THIS IS THE PROBLEM! WE ARE SAVING DATA IN $0000 AND $0001!!! since we set x to be 00 at some point
-    ldx #$80
-    ;dex
-    ;dex
-    lda #>string_debug_command      ; High byte
-    sta 1, x
-    lda #<string_debug_command      ; Low byte
-    sta 0, x
-    jsr print_string
-    ;inx                             ; Free up data stack
-    ;inx
-    lda #(%10000000 | LCD_SECOND_LINE)
-    jsr lcd_send_command
-
+main_print:
     lda #'$'
     jsr lcd_write_char
-    lda COMMAND
+    jsr acia_send_char
+    ldx COM_BUF_START
+    lda COM_BUF, x
+    sec
     jsr print_hex_value
     lda #' '
     jsr lcd_write_char
-
-    lda #'$'
-    jsr lcd_write_char
-    lda COMMAND_ADDRESS + 1
-    jsr print_hex_value
-    lda COMMAND_ADDRESS
-    jsr print_hex_value
-
-    lda #'$'
-    jsr lcd_write_char
-    tya
-    jsr print_hex_value
-    rts
-
-main_store_command:
-    ldx COM_BUF_START
-    lda COM_BUF, x
-    sta COMMAND
-    jmp main_loop
-
-main_store_address:
-    ;ldx COM_BUF_START
-    ;lda COM_BUF, x
-    ;dey                             ; y is either 1 or 2, but offset should be 0 or 1
-    ;sta COMMAND_ADDRESS, y          ; Store acc in COMMAND_ADDRESS + y - #$01 (Does this work on ZP?)
-    ;iny
-    tya
-    pha
-    tax
-    dex
-    ldy COM_BUF_START
-    lda COM_BUF, y
-    sta COMMAND_ADDRESS, x
-    pla
-    tay
-    jmp main_loop
-
-command_rdram:
-    ldx #$00
-command_rdram_loop:
-    lda (COMMAND_ADDRESS, x)
     jsr acia_send_char
-    inx
-    cpx #$10
-    bne command_rdram_loop
-    ldy #$00                        ; Time for a new round
-    jmp main_loop
+    inc COM_BUF_START
+    iny
+    jmp main
+
+
+;    ; y=0: store byte in COMMAND_ADDRESS
+;    ; 0<y<=2: store byte in COMMAND
+;    ; y>3:  store byte in memory COMMAND_ADDRESS, y
+;    ;tya
+;    cpy #$00
+;    beq main_store_command
+;    cpy #$03
+;    bmi main_store_address
+;main_handle_data:
+;    lda COMMAND
+;    cmp #COMMAND_RDRAM
+;    beq command_rdram
+;
+;main_loop:
+;    inc COM_BUF_START               ; Update COM_BUF_START
+;    iny
+;    jsr main_print_status
+;    cpy #$13
+;    bmi main
+;    ldy #$00                        ; If y > 12 we roll around to #$00
+;    jsr main_print_status
+;    jmp main
+;
+;main_print_status:
+;; Print current command, address, and y
+;    lda #%00000001                  ; Clear display
+;    jsr lcd_send_command
+;    lda #%00000010                  ; Return cursor home
+;    jsr lcd_send_command
+;
+;    ; THIS IS THE PROBLEM! WE ARE SAVING DATA IN $0000 AND $0001!!! since we set x to be 00 at some point
+;    ldx #$80
+;    ;dex
+;    ;dex
+;    lda #>string_debug_command      ; High byte
+;    sta 1, x
+;    lda #<string_debug_command      ; Low byte
+;    sta 0, x
+;    jsr print_string_zp
+;    ;inx                             ; Free up data stack
+;    ;inx
+;    lda #(%10000000 | LCD_SECOND_LINE)
+;    jsr lcd_send_command
+;
+;    lda #'$'
+;    jsr lcd_write_char
+;    lda COMMAND
+;    jsr print_hex_value
+;    lda #' '
+;    jsr lcd_write_char
+;
+;    lda #'$'
+;    jsr lcd_write_char
+;    lda COMMAND_ADDRESS + 1
+;    jsr print_hex_value
+;    lda COMMAND_ADDRESS
+;    jsr print_hex_value
+;
+;    lda #'$'
+;    jsr lcd_write_char
+;    tya
+;    jsr print_hex_value
+;    rts
+;
+;main_store_command:
+;    ldx COM_BUF_START
+;    lda COM_BUF, x
+;    sta COMMAND
+;    jmp main_loop
+;
+;main_store_address:
+;    ;ldx COM_BUF_START
+;    ;lda COM_BUF, x
+;    ;dey                             ; y is either 1 or 2, but offset should be 0 or 1
+;    ;sta COMMAND_ADDRESS, y          ; Store acc in COMMAND_ADDRESS + y - #$01 (Does this work on ZP?)
+;    ;iny
+;    tya
+;    pha
+;    tax
+;    dex
+;    ldy COM_BUF_START
+;    lda COM_BUF, y
+;    sta COMMAND_ADDRESS, x
+;    pla
+;    tay
+;    jmp main_loop
+;
+;command_rdram:
+;    ldx #$00
+;command_rdram_loop:
+;    lda (COMMAND_ADDRESS, x)
+;    jsr acia_send_char
+;    inx
+;    cpx #$10
+;    bne command_rdram_loop
+;    ldy #$00                        ; Time for a new round
+;    jmp main_loop
+
 
 ; ----------------------------------------
-; ----- Subroutines & interrupts ---------
+; ----- Interrupts -----------------------
 ; ----------------------------------------
 interrupt:
     rti
@@ -249,6 +275,7 @@ non_maskable_interrupt:
 ;standard_interrupt:
     tya
     pha
+
     jsr acia_receive_char               ; Read char if available
     bcc return_from_standard_interrupt  ; Return if no char available
     ldx COM_BUF_END
@@ -280,20 +307,99 @@ string_debug_command:
 ;         "$aa $abcd $01"
 
 ; ----------------------------------------
-; ----- Common Subroutines ---------------
+; ----- Subroutines ----------------------
 ; ----------------------------------------
     .segment "SUBROUTINES"
 
+; Protocol
+; CMD, ADDRx2, DLEN, DATA...
+; Length is 4 + d bytes
+
+; Start parsing new message if the buffer is >3 (and "CMDRDY"-flag is clear)
+; Put the CMD, ADDR and DLEN in variables
+; We continue to read all data (then we set "CMDRDY"-flag)
+
+; If CMDRDY is set we can take action on the message
+; (When we are done with the message we set the CMDRDY-flag to zero)
+read_command:
+    pha
+    txa
+    pha
+    lda COM_BUF_END
+    sec
+    sbc COM_BUF_START
+    sec
+    sbc #PROTOCOL_HEADER_LEN
+    bmi read_command                    ; Branch if COM_BUF_START >= COM_BUF_END
+    ldx #PROTOCOL_HEADER_LEN - 1
+store_command:
+    lda COM_BUF_START, x
+    sta COMMAND, x
+    dex
+    bpl store_command
+    lda COM_BUF_START
+    adc #PROTOCOL_HEADER_LEN
+    sta COM_BUF_START
+
+
+; Here we need to wait for all data bytes to arrive
+    lda COMMAND_DATALEN
+    beq read_command_return             ; Branch on zero
+    tax
+    dex
+store_command_data:
+    lda COM_BUF_START, x
+    sta COMMAND_DATA, x
+    dex
+    bpl store_command_data
+read_command_return:
+    pla
+    tax
+    pla
+    sei                                 ; Debugging
+    rts
+
+write_command:
+    pha
+    txa
+    pha
+    ldx #$00
+write_command_to_acia:
+    lda COMMAND, x
+    jsr acia_send_char
+    inx
+    cpx PROTOCOL_HEADER_LEN
+    bne write_command_to_acia
+    lda COMMAND_DATALEN
+    beq write_command_return
+    ldx #$00
+write_command_data_to_acia:
+    lda COMMAND_DATA, x
+    jsr acia_send_char
+    inx
+    cpx COMMAND_DATALEN
+    bne write_command_data_to_acia
+write_command_return:
+    pla
+    tax
+    pla
+    rts
+
+
 print_string:
-    lda ($00, x)  ; Finds the address at top of the data stack and loads the accumulator with value from that address
+; Prints a string whose pointer is located in address TEMP_ADDRESS. y is where to start printing. Currently uses both a, x and y
+    ldx PRINT_HEX_TO_ACIA
+print_string_loop:
+    lda (TEMP_ADDRESS), y
     cmp #$00
     beq print_string_return
+    cpx #$00
+    beq print_string_no_acia
+    jsr acia_send_char
+print_string_no_acia:
     jsr lcd_write_char
-    ;jsr acia_send_char
-    inc $00, x
-    bne print_string
-    inc $01, x
-    bne print_string
+    iny
+    jmp print_string_loop
 print_string_return:
     rts
 
@@ -314,12 +420,11 @@ print_hex_value:
     pha
     tya
     pha
-
     tsx
     inx
     inx
     inx
-    lda STACK, x            ; Get back accumulator
+    lda STACK, x                    ; Get back accumulator
     lsr
     lsr
     lsr
@@ -328,13 +433,19 @@ print_hex_value:
     tay
     lda table_byte_to_char, y
     jsr lcd_write_char
-
-    lda STACK, x            ; Get back accumulator
+    ldy PRINT_HEX_TO_ACIA
+    beq print_hex_value_no_acia_1   ; If carry bit is set we send to ACIA
+    jsr acia_send_char
+print_hex_value_no_acia_1:
+    lda STACK, x                    ; Get back accumulator
     and #%00001111
     tay
     lda table_byte_to_char, y
     jsr lcd_write_char
-
+    ldy PRINT_HEX_TO_ACIA
+    beq print_hex_value_no_acia_2   ; If carry bit is set we send to ACIA
+    jsr acia_send_char
+print_hex_value_no_acia_2:
     pla
     tay
     pla
