2d323f5eb3d0d12796ea4f98db3cee9a69e20d07
filip-we <filip.wettlen@gmail.com>
2021-09-05 21:57:17 +0200

    Reading SPI and printing to screen!

--- src/mcp2210_spi.py
+++ src/mcp2210_spi.py
@@ -10,5 +10,17 @@ log.addHandler(stdout_handler)
 
 chip = Mcp2210(serial_number='0001101309')
 
+print('\n')
+print(chip._spi_settings.mode)
+
+chip.set_gpio_designation(4, Mcp2210GpioDesignation.CHIP_SELECT)
+tx_data = bytes(range(16))
+rx_data = chip.spi_exchange(tx_data, cs_pin_number=4)
+assert rx_data == tx_data
+print(tx_data)
+print(rx_data)
+
+#chip.transfer(data)
+
 
 
