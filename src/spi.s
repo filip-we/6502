2307c2dc6ee08c0860a13fdd8529fa8068898b6d
filip-we <filip.wettlen@gmail.com>
2021-09-06 15:30:06 +0200

    WIP

--- src/spi.s
+++ src/spi.s
@@ -6,11 +6,21 @@
 ;   Other stuff goes here
    .include "via1.s"
 
-    SPI_DATA = $1F00
+    SPI_PORT = PORTA
+    SPI_MOSI_PORT_BIT = %01000000
+    SPI_MISO_PORT_BIT = %10000000
 
-    SPI_WRITE = $00
-    SPI_READ = $01
-    LCD_COUNT = $02
+
+; Variables in RAM
+    SPI_MOSI_WRITE = $00
+    SPI_MOSI_READ = $01
+    SPI_MOSI_INCOMMING_BYTE = $02
+    SPI_CURRENT_BIT = $05
+
+    LCD_COUNT = $10
+
+    SPI_MOSI_DATA = $1F00
+
 
 string_s:
     .byte "=== SPI  ===", $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
@@ -46,8 +56,8 @@ start:
 
 ; Reset variables
     lda $00
-    sta SPI_WRITE
-    sta SPI_READ
+    sta SPI_MOSI_WRITE
+    sta SPI_MOSI_READ
     sta LCD_COUNT
 
 ; Print a string
@@ -64,17 +74,17 @@ main_lcd_first_line:
     sta LCD_COUNT
 
 main_loop:
-    lda SPI_READ
-    cmp SPI_WRITE
+    lda SPI_MOSI_READ
+    cmp SPI_MOSI_WRITE
     bpl main_loop
 
     lda #'$'
     jsr lcd_write_char
 
-    ldx SPI_READ
-    lda SPI_DATA, x
+    ldx SPI_MOSI_READ
+    lda SPI_MOSI_DATA, x
     jsr print_hex_value
-    inc SPI_READ
+    inc SPI_MOSI_READ
     inc LCD_COUNT
 
     lda #' '
@@ -93,6 +103,13 @@ main_lcd_second_line:
     jsr lcd_send_command
     jmp main_loop
 
+shift_register_read:
+    ldx SPI_MOSI_WRITE
+    lda SR
+    sta SPI_MOSI_DATA, x
+    inc SPI_MOSI_WRITE
+    rts
+
 interrupt:
     sei
     pha
@@ -102,11 +119,7 @@ interrupt:
     lda IFR
     and #%00000100
     beq return_interrupt        ; Only caring about Shift Registers
-
-    ldx SPI_WRITE
-    lda SR
-    sta SPI_DATA, x
-    inc SPI_WRITE
+    jsr shift_register_read
 
 return_interrupt:
     pla
@@ -115,3 +128,32 @@ return_interrupt:
     cli
     rti
 
+
+; Bit-banged SPI slave
+    lda #$00
+    sta SPI_CURRENT_BIT
+    sta SPI_MOSI_INCOMMING_BYTE
+
+
+spi_via_tx_rx:
+; Run with every SCK-pulse
+    clc
+    lda SPI_PORT
+    and SPI_MOSI_PORT_BIT
+    beq spi_mosi_zero
+    sec
+spi_mosi_zero:
+    rol SPI_MOSI_INCOMMING_BYTE
+    lda SPI_CURRENT_BIT         ; If this is zero we have received the whole byte
+    bne spi_via_tx_rx_return
+
+    lda SPI_MOSI_INCOMMING_BYTE
+    ldx SPI_MOSI_WRITE
+    sta SPI_MOSI_DATA, x
+    inc SPI_MOSI_WRITE
+    lda #$08                    ; Next rond we start a new byte.
+    sta SPI_CURRENT_BIT
+
+spi_via_tx_rx_return:
+    dec SPI_CURRENT_BIT         ; Update bit number
+    rts
