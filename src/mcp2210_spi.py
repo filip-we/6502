7f6750669c07e1d8f319d79c476b623461254311
filip-we <filip.wettlen@gmail.com>
2021-09-06 21:19:57 +0200

    WIP

--- src/mcp2210_spi.py
+++ src/mcp2210_spi.py
@@ -4,23 +4,21 @@ import sys
 from mcp2210 import Mcp2210, Mcp2210GpioDesignation, Mcp2210GpioDirection
 
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 stdout_handler = logging.StreamHandler(sys.stdout)
 log.addHandler(stdout_handler)
 
 chip = Mcp2210(serial_number='0001101309')
 
-print('\n')
 print(chip._spi_settings.mode)
 
 chip.set_gpio_designation(4, Mcp2210GpioDesignation.CHIP_SELECT)
-tx_data = bytes(range(16))
+tx_data = bytes(range(3))
 rx_data = chip.spi_exchange(tx_data, cs_pin_number=4)
-assert rx_data == tx_data
 print(tx_data)
 print(rx_data)
 
-#chip.transfer(data)
+assert rx_data == tx_data
 
 
 
--- src/spi.s
+++ src/spi.s
@@ -7,23 +7,27 @@
    .include "via1.s"
 
     SPI_PORT = PORTA
-    SPI_MOSI_PORT_BIT = %01000000
-    SPI_MISO_PORT_BIT = %10000000
+    SPI_MOSI_PORT_BIT = %10000000
+    SPI_MISO_PORT_BIT = %00000010
 
 
 ; Variables in RAM
-    SPI_MOSI_WRITE = $00
-    SPI_MOSI_READ = $01
-    SPI_MOSI_INCOMMING_BYTE = $02
-    SPI_CURRENT_BIT = $05
+    SPI_MOSI_CURRENT_BIT = $00
+    SPI_MOSI_WRITE = $01
+    SPI_MOSI_READ = $02
+    SPI_MOSI_INCOMMING_BYTE = $03
+
+    SPI_MISO_CURRENT_BIT = $05
+    SPI_MISO_WRITE = $06
+    SPI_MISO_READ = $07
 
     LCD_COUNT = $10
+    temp = $11
 
     SPI_MOSI_DATA = $1F00
+    SPI_MISO_DATA = $2000
 
 
-string_s:
-    .byte "=== SPI  ===", $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
 
 start:
     sei                     ; Set interrupt flag (== interrupts ignored)
@@ -35,9 +39,9 @@ start:
     sta DDRB
     lda #%00001100          ; Set Shift Register to shift in with external clock
     sta ACR
-    lda #%00000000          ; Set CB2-controls to input, negative active edge.
+    lda #%00000000          ; Set CB/CA-controls to input, negative active edge.
     sta PCR
-    lda #%10000100          ; Enable SR-interrupt
+    lda #%10000011          ; Enable CA1 & 2
     sta IER
     lda SR                  ; Clear content of SR
 
@@ -56,13 +60,26 @@ start:
 
 ; Reset variables
     lda $00
+    sta SPI_MOSI_CURRENT_BIT
     sta SPI_MOSI_WRITE
     sta SPI_MOSI_READ
+    sta SPI_MOSI_INCOMMING_BYTE
+    sta SPI_MISO_CURRENT_BIT
+    sta SPI_MISO_WRITE
+    sta SPI_MISO_READ
+
     sta LCD_COUNT
+    lda #'a'
+    sta temp
 
 ; Print a string
     lda #'!'
     jsr lcd_write_char
+
+; Prime MISO-buffer
+    lda #18
+    lda SPI_MISO_WRITE
+
 ; Finishing reset
     cli                     ; Clear Interrupt disable (i.e. listen for interrupts)
 
@@ -74,6 +91,7 @@ main_lcd_first_line:
     sta LCD_COUNT
 
 main_loop:
+    jmp main_loop               ; Disable print to see IRQ behaviour
     lda SPI_MOSI_READ
     cmp SPI_MOSI_WRITE
     bpl main_loop
@@ -103,23 +121,85 @@ main_lcd_second_line:
     jsr lcd_send_command
     jmp main_loop
 
-shift_register_read:
-    ldx SPI_MOSI_WRITE
-    lda SR
-    sta SPI_MOSI_DATA, x
-    inc SPI_MOSI_WRITE
-    rts
-
 interrupt:
     sei
     pha
     txa
     pha
 
+    ;lda temp
+    ;jsr print_hex_value
+    ;inc temp
+
+check_spi_miso:
     lda IFR
-    and #%00000100
-    beq return_interrupt        ; Only caring about Shift Registers
-    jsr shift_register_read
+    pha
+    jsr print_hex_value
+
+    tsx
+    inx
+    lda __STACK_START__, x
+    jsr print_hex_value
+    and #%00000010              ; Checking CA1 Interrupts first to do some things every SPI-clock cycle
+    beq return_interrupt        ; If not CA1 we cannot have received any MOSI, so quit
+
+    lda #'>'
+    jsr lcd_write_char
+
+    inc SPI_MOSI_CURRENT_BIT
+    lda SPI_MOSI_CURRENT_BIT
+    cmp $08
+    bne check_if_miso_data
+    lda $00
+    sta SPI_MOSI_CURRENT_BIT
+
+check_if_miso_data:
+    lda SPI_MISO_WRITE
+    cmp SPI_MISO_READ
+    beq spi_miso_clear_flag     ; If they are equal we have nothing to send
+; Check if it is OK to start a new byte (ie SPI_MISO_CURRENT_BIT + 1 == SPI_MOSI_CURRENT_BIT )
+    lda #'I'
+    jsr lcd_write_char
+
+    lda SPI_MISO_CURRENT_BIT
+    adc #$01                    ; We are one byte ahead because it needs to be ready when the next clock pulse comes
+    and #%00000111              ; To catch $08
+    cmp SPI_MOSI_CURRENT_BIT
+    bne spi_miso_clear_flag     ; Cannot send
+
+    ldx SPI_MISO_READ
+    lda SPI_MISO_DATA, x
+    and SPI_MOSI_CURRENT_BIT         ; Acc now has next bit to send
+    beq spi_miso_send_zero
+    lda SPI_MISO_PORT_BIT
+    sta SPI_PORT
+spi_miso_send_zero:
+    lda $00
+    sta SPI_PORT
+    inc SPI_MISO_READ
+
+    lda #'W'
+    jsr lcd_write_char
+
+spi_miso_clear_flag:
+    ;lda IFR                     ; Clear IFR CA1 by writing that bit
+    ;and #%11111100
+    ;sta IFR
+
+check_spi_mosi:
+    tsx
+    inx
+    lda __STACK_START__, x
+    and #%00000100              ; Shift Register Interrupt
+    beq return_interrupt
+
+    lda #'O'
+    jsr lcd_write_char
+
+    ldx SPI_MOSI_WRITE
+    lda SR
+    sta SPI_MOSI_DATA, x
+    inc SPI_MOSI_WRITE
 
 return_interrupt:
     pla
@@ -129,14 +209,8 @@ return_interrupt:
     rti
 
 
-; Bit-banged SPI slave
-    lda #$00
-    sta SPI_CURRENT_BIT
-    sta SPI_MOSI_INCOMMING_BYTE
-
-
 spi_via_tx_rx:
-; Run with every SCK-pulse
+; NOT TESTED
     clc
     lda SPI_PORT
     and SPI_MOSI_PORT_BIT
@@ -144,7 +218,7 @@ spi_via_tx_rx:
     sec
 spi_mosi_zero:
     rol SPI_MOSI_INCOMMING_BYTE
-    lda SPI_CURRENT_BIT         ; If this is zero we have received the whole byte
+    lda SPI_MOSI_CURRENT_BIT         ; If this is zero we have received the whole byte
     bne spi_via_tx_rx_return
 
     lda SPI_MOSI_INCOMMING_BYTE
@@ -152,8 +226,12 @@ spi_mosi_zero:
     sta SPI_MOSI_DATA, x
     inc SPI_MOSI_WRITE
     lda #$08                    ; Next rond we start a new byte.
-    sta SPI_CURRENT_BIT
+    sta SPI_MOSI_CURRENT_BIT
 
 spi_via_tx_rx_return:
-    dec SPI_CURRENT_BIT         ; Update bit number
+    dec SPI_MOSI_CURRENT_BIT         ; Update bit number
     rts
+
+    .org SPI_MISO_DATA          ; Priming some data to send
+string_s:
+    .byte "=== Iroko 0.2  ===", $00
