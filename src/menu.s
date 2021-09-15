23483c3e53b3440cde7621cc20f7ccd22341d8bd
filip-we <filip.wettlen@gmail.com>
2021-09-15 22:46:09 +0200

    Added framework for button reading and debouncing

--- src/menu.s
+++ src/menu.s
@@ -10,6 +10,11 @@
     ADDR_A          = ZP_START + $10
     ADDR_B          = ZP_START + $12
 
+
+    BUTTON_1        = ZP_START + $20
+    BUTTON_2        = ZP_START + $21
+    BUTTON_3        = ZP_START + $22
+
     TEMP            = $0600
 
 start:
@@ -37,6 +42,13 @@ start:
     lda #LCD_DISPLAY_ON
     jsr lcd_command
 
+; Reset variables
+    ldx ZP_START
+reset_loop:
+    sta $00, x
+    inx
+    bne reset_loop
+
 ; Print start message
     lda #<test_text         ; Low byte
     sta ADDR_A
@@ -51,9 +63,32 @@ main_loop:
 read_buttons:
 ; Configure VIA to interrupt at regular intervals
 ; On interrupt, read status of all buttons. 
-;   For each button: compare state with flag in memory location. if flag 1 and  button 1 increase counter. If states are opposite zero counter and set flag.
+;   For each button: compare state with flag in memory location.
+;   If Flag==1 and  button==1: increase counter.
+;   If Flag==1 and  button==0: reset counter.
+;   (If Flag==0 and  button==0: increase counter.)
+;   If Flag==0 and  button==1: reset counter.
 ;   Flag 1: Check if counter > 10: store key in keyboardbuffer. Reset counter.
-;   Flag 0: Check if counter > 100: store release key in keyboardbuffer. Reset counter.
+;   (Flag 0: Check if counter > 100: store release key in keyboardbuffer. Reset counter.) Can be done later
+;
+;   Byte: flag, nnn nnnn
+;   Flag not needed now since we dont count non-button-presses
+;   Garth says: BIT puts bit 7's value in the N flag
+    lda PORTA
+    and #BUTTON_1_PIN
+    bne button_not_pressed          ; We don't care if the button is not pressed
+    inc BUTTON_1
+    lda #$f0
+    cmp BUTTON_1
+    bne button_return
+    ; Add to buffer and update pointer
+    rts
+
+button_not_pressed:
+    lda #0                          ;   We don't count how long the button is NOT pressed
+    sta BUTTON_1
+button_return:
+    rts
 
 nmi:
     rti
